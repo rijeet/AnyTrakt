826ff8fb77bce458cc9aaa56f4251c123ea9743c
You-sen <88145204+You-sen@users.noreply.github.com>
2023-08-27 00:51:13 +0600

    movies list and status controller

---
+++ src/main/java/com/example/anytrakt/UserMovieStatusTableController.java
@@ -0,0 +1,134 @@
+package com.example.anytrakt;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class UserMovieStatusTableController implements Initializable {
+
+
+
+    @FXML
+    private TableColumn<Watch_List_OnStatus,String> Genre_col;
+
+    @FXML
+    private TableColumn<Watch_List_OnStatus,String> IMDb_Rating_col;
+
+    @FXML
+    private TableView<Watch_List_OnStatus> Movie_Table;
+
+    @FXML
+    private TableColumn<Watch_List_OnStatus,String> OnStatus_col;
+
+    @FXML
+    private TableColumn<Watch_List_OnStatus,String> Release_Year_col;
+
+    @FXML
+    private Button Search_btn;
+
+    @FXML
+    private ComboBox<String> Search_type_comboBox;
+    ObservableList<String> columntpye = FXCollections.observableArrayList("Genre","Title");
+
+    @FXML
+    private TextField Search_value;
+
+    @FXML
+    private TableColumn<Watch_List_OnStatus,String> Title_col;
+
+
+    static ObservableList<Watch_List_OnStatus> UserOnStatusMovielist= FXCollections.observableArrayList();
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Search_type_comboBox.setItems(columntpye);
+        Movie_Table.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        DB_Connection dbAction = new DB_Connection();
+        try{
+            UserMovieStatusTableController.UserOnStatusMovielist=  dbAction.getAllMovieOnStatus();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Logger.getLogger(UserMovieStatusTableController.class.getName()).log(Level.SEVERE, null, e);
+        }
+
+        Title_col.setCellValueFactory(new PropertyValueFactory<Watch_List_OnStatus,String>("Title"));
+//Author
+        Release_Year_col.setCellValueFactory(new PropertyValueFactory<Watch_List_OnStatus,String>("Release_Year"));
+        Genre_col.setCellValueFactory(new PropertyValueFactory<Watch_List_OnStatus,String>("Genre"));
+        IMDb_Rating_col.setCellValueFactory(new PropertyValueFactory<Watch_List_OnStatus,String>("IMDb_Rating"));
+        OnStatus_col.setCellValueFactory(new PropertyValueFactory<Watch_List_OnStatus,String>("OnStatus"));
+
+        Movie_Table.setItems(UserMovieStatusTableController.UserOnStatusMovielist);
+    }
+
+
+    @FXML
+    void Delete_button_Action_User_movie_Status(ActionEvent event) throws SQLException{
+        ObservableList<Watch_List_OnStatus> selectedMovie=FXCollections.observableArrayList();
+        selectedMovie=Movie_Table.getSelectionModel().getSelectedItems();
+        DB_Connection dbAction=new DB_Connection();
+        dbAction.deleteOnStatusUserMovie(selectedMovie);
+        UserMovieStatusTableController.UserOnStatusMovielist.removeAll(selectedMovie);
+    }
+
+    @FXML
+    void GroupByCompleted(ActionEvent event) throws SQLException{
+
+        ObservableList<Watch_List_OnStatus> selectedMovie=FXCollections.observableArrayList();
+        DB_Connection dbAction=new DB_Connection();
+        selectedMovie =  dbAction.getAllMovieGroupByCompleted();
+
+        Movie_Table.setItems(selectedMovie);
+
+
+    }
+
+    @FXML
+    void GroupByPlan_To_Watch(ActionEvent event) throws SQLException{
+
+        ObservableList<Watch_List_OnStatus> selectedMovie=FXCollections.observableArrayList();
+        DB_Connection dbAction=new DB_Connection();
+        selectedMovie =  dbAction.getAllMovieGroupByPlan_To_Watch();
+
+        Movie_Table.setItems(selectedMovie);
+
+    }
+
+    @FXML
+    void GroupByWatch(ActionEvent event) throws SQLException{
+
+        ObservableList<Watch_List_OnStatus> selectedMovie=FXCollections.observableArrayList();
+        DB_Connection dbAction=new DB_Connection();
+        selectedMovie =  dbAction.getAllMovieGroupByWatching();
+
+        Movie_Table.setItems(selectedMovie);
+
+    }
+
+    @FXML
+    void Search_button_Action_User_movie_Status(ActionEvent event) throws SQLException{
+        String Type=Search_type_comboBox.getValue();
+        String value=Search_value.getText();
+        ObservableList<Watch_List_OnStatus> selectedMovie=FXCollections.observableArrayList();
+        DB_Connection dbAction=new DB_Connection();
+        selectedMovie  = dbAction.searchOnStatusMovie(Type, value);
+
+
+        Movie_Table.setItems(selectedMovie);
+    }
+}
