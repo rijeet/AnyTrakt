aac541fe85cae4d89d0b46cad729c479f792a329
Rijeet Bin Anis <risan.aust@gmail.com>
2023-09-01 18:55:17 +0600

    Update MovieListTableController.java

--- src/main/java/com/example/anytrakt/MovieListTableController.java
+++ src/main/java/com/example/anytrakt/MovieListTableController.java
@@ -9,8 +9,8 @@ import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
-
 import java.net.URL;
+
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
