fa8891d2e853d7450a19da0dbae675e86fc7cdb7
Rijeet Bin Anis <risan.aust@gmail.com>
2023-09-01 19:06:19 +0600

    Update CreateAccountController.java

--- src/main/java/com/example/anytrakt/CreateAccountController.java
+++ src/main/java/com/example/anytrakt/CreateAccountController.java
@@ -8,8 +8,8 @@ import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
-
 import java.io.IOException;
+
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
